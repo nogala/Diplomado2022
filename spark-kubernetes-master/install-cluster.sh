1052c88c28af7bc64c0b0b375f88cdf40661c728
Norberto Garcia <Norberto.Garcia@bigfishgames.com>
2022-06-30 23:28:53 -0500

    Update install-cluster.sh

--- spark-kubernetes-master/install-cluster.sh
+++ spark-kubernetes-master/install-cluster.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ## Config memory and CPUs from local resources
 minikube start --memory 8192 --cpus 4
+## Point the shell to minikube's Docker daemon.
+eval $(minikube docker-env)
 ## Create docker spark image
 docker build -f docker/Dockerfile -t spark-hadoop:3.2.0 ./docker
+kubeclt create ns spark-demo
+kubens spark-demo
 ## Deploy master
 kubectl create -f ./kubernetes/spark-master-deployment.yaml
 kubectl create -f ./kubernetes/spark-master-service.yaml
@@ -16,6 +20,6 @@ kubectl apply -f ./kubernetes/minikube-ingress.yaml
 echo "$(minikube ip) spark-kubernetes"
 ## get nodes to connect master
 kubectl get pods -o wide
+# kubectl exec [TODO: ADD-MASTER-SPARK-HERE] -it -- pyspark --conf spark.driver.bindAddress=172.17.0.6 --conf spark.driver.host=172.17.0.6
 ## Create dashboard
 minikube dashboard
-minikube dashboard -url 
