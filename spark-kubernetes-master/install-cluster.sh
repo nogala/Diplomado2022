41de3bf9b8a937da05e6855647b2000a6fad6783
Norberto Garcia <Norberto.Garcia@bigfishgames.com>
2022-07-01 01:47:18 -0500

    fix

--- spark-kubernetes-master/install-cluster.sh
+++ spark-kubernetes-master/install-cluster.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 ## Config memory and CPUs from local resources
-minikube start --memory 8192 --cpus 4
+minikube start --cpus 4 --memory 8192
+# kubectl cluster-info
+# kubectl cluster view
+kubectl get pods -A
+eval $(minikube -p minikube docker-env)
 ## Point the shell to minikube's Docker daemon.
 eval $(minikube docker-env)
 ## Create docker spark image
-docker build -f docker/Dockerfile -t spark-hadoop:3.2.0 ./docker
-kubeclt create ns spark-demo
-kubens spark-demo
+docker build -f docker/Dockerfile -t spark:3.3.0 ./docker
+kubectl create ns spark
+kubectl config set-context --current --namespace=spark
 ## Deploy master
 kubectl create -f ./kubernetes/spark-master-deployment.yaml
 kubectl create -f ./kubernetes/spark-master-service.yaml
@@ -18,8 +22,9 @@ kubectl get pods
 minikube addons enable ingress
 kubectl apply -f ./kubernetes/minikube-ingress.yaml
 echo "$(minikube ip) spark-kubernetes"
-## get nodes to connect master
+## get nodes to connect worker
 kubectl get pods -o wide
-# kubectl exec [TODO: ADD-MASTER-SPARK-HERE] -it -- pyspark --conf spark.driver.bindAddress=172.17.0.6 --conf spark.driver.host=172.17.0.6
+# kubectl exec spark-worker-fc4d55c87-8rrbd -it -- pyspark 
+## --conf spark.driver.bindAddress=172.17.0.9 --conf spark.driver.host=172.17.0.9
 ## Create dashboard
 minikube dashboard
